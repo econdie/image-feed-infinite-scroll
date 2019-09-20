c9d6b4dd80c0fd3c1ba79dd89f64b21422a2ed63
econdie <erik@7cups.com>
2019-09-19 23:25:29 -0400

    add build and start script

--- www/components/page/nav.js
+++ www/components/page/nav.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 const links = [
   { href: "/", label: "Home" },
-  { href: "https://github.com/econdie", label: "GitHub" }
+  {
+    href: "hhttps://github.com/econdie/image-feed-infinite-scroll/",
+    label: "GitHub"
+  }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`;
   return link;
